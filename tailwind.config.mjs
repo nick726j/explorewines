faa43ed3131bfb1e2922096db9c1cd04e95d2268
Nicklas <nicklasahrnberg@gmail.com>
2024-12-18 11:08:50 +0100

    pushing

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,25 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
-
-  /* Vil du overskrive eller extende tailwinds indstillinger?  */
-
   theme: {
-    /* Tilføj din egen farvepalette nedenfor */
+    /* Tilføj din egen farvepalette */
     colors: {
       transparent: "transparent",
       current: "currentColor",
       creamywhite: "#FFFFF5",
       orange: "#BF602C",
-      brown: "#3F2C22",
+      brown: "#3F2C22" /* Brun farve */,
+      white: "#FFFFFF",
+      black: "#000000",
     },
-
-    /* Tilføj din egen spacing nedenfor */
-    // spacing: {
-    //     'm': '40px',
-    // },
-
-    /* Tilføj fonte herunder - husk også at tilføje webfonte i head-sektionen i MainLayout  */
     fontFamily: {
       serif: ["neue-haas-grotesk-display"],
       display: ["neue-haas-grotesk-display"],
@@ -28,9 +20,9 @@ export default {
     /* Tilføj dit typografiske hierarki herunder */
     fontSize: {
       xs: "0.6rem",
-      s: "0.9rem",
-      m: "1.4rem",
-      l: "1.6rem",
+      s: "0.85rem",
+      m: "1rem",
+      l: "1.2rem",
 
       xsmb: "0.4rem",
       smb: "0.6rem",
@@ -50,78 +42,15 @@ export default {
       medium: "600",
     },
 
-    /* Borders - borderstørrelser herunder */
-    // borderWidth: {
-    //     DEFAULT: '1px',
-    //     0: '0px',
-    //     2: '2px',
-    // },
-
-    /* Border radius størrelser herunder */
-    // borderRadius: {
-    //     DEFAULT: '1.25rem',
-    //     none: '0px',
-    // },
-
-    /* Box shadows herunder */
-    // boxShadow: {
-    //     drop25: '4px 4px 4px 0 rgb(0 0 0 / 0.25)', /* x y blur spread farve / opacity */
-    // },
-
     extend: {
-      /* Tilføj din egen farvepalette nedenfor */
-      // colors: {
-      //     transparent: 'transparent',
-      //     current: 'currentColor',
-      //     'farvenavn': {
-      //         50: '#hexkode',
-      //         100: '#hexkode',
-      //         200: '#hexkode',
-      //         300: '#hexkode',
-      //         400: '#hexkode',
-      //         500: '#hexkode',
-      //         600: '#hexkode',
-      //         700: '#hexkode',
-      //         800: '#hexkode',
-      //         900: '#hexkode',
-      //     },
-      //     'white': '#fff',
-      //     'black': '#000',
-      // },
-      /* Tilføj din egen spacing nedenfor */
-      // spacing: {
-      //     'm': '40px',
-      // },
-      /* Tilføj fonte herunder - husk også at tilføje webfonte i head-sektionen i MainLayout  */
-      // fontFamily: {
-      //     sans: ['Prompt', 'sans-serif'],
-      //     'display': ['Courier', 'monospace'], /* eksempel på ekstra font */
-      // },
-      /* Tilføj dit typografiske hierarki herunder */
-      // fontSize: {
-      //     base: ['1rem', { lineHeight: '1.5rem' }],
-      //     large: ['2.5rem', { lineHeight: '3.75rem' }],
-      //   },
-      /* Tilføj dit fontvægt-hierarki nedenfor */
-      //   fontWeight: {
-      //     regular: '400',
-      //   },
-      /* Borders - borderstørrelser herunder */
-      // borderWidth: {
-      //     DEFAULT: '1px',
-      //     0: '0px',
-      //     2: '2px',
-      // },
-      /* Border radius størrelser herunder */
-      // borderRadius: {
-      //     DEFAULT: '1.25rem',
-      //     none: '0px',
-      // },
-      /* Box shadows herunder */
-      // boxShadow: {
-      //     drop25: '4px 4px 4px 0 rgb(0 0 0 / 0.25)', /* x y blur spread farve / opacity */
-      // },
+      colors: {
+        base: {
+          text: "#3F2C22" /* Brun som base tekstfarve */,
+          background: "FFFFF5" /* Lys baggrund */,
+        },
+      },
     },
+    /* Resten af konfigurationen */
   },
   plugins: [require("@tailwindcss/typography"), require("@tailwindcss/forms"), require("@tailwindcss/aspect-ratio"), require("@tailwindcss/container-queries")],
 };
